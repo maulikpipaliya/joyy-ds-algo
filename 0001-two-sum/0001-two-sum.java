6555b04b4d486a012b4ad03f5e53305378f7f452
Maulik Pipaliya <maulik.pipaliya@gmail.com>
2024-08-04 20:08:38 +0530

    Time: 16 ms (53.03%), Space: 45.8 MB (8.56%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int len = nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for(int i=0;i<len;i++){
+            map.put(nums[i], i);   
+        }
+        
+        System.out.println(map);
+        
+        for(int i=0;i<len;i++){
+            Integer x = target - nums[i];
+            Integer indexOfX = map.get(x);
+            if(indexOfX != null && indexOfX != i){
+                return new int[]{i, indexOfX};
+            }
+        }
+        
+        return new int[]{};
+    }
+}
